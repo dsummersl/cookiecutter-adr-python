ec7ffa262b653930e4fbdb72a00a3bf49cd85938
Dane Summers <dsummersl@yahoo.com>
2025-09-05 15:57:31 -0400

    Add github workflow

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,19 +1,7 @@
 import os
 import subprocess
 
-
-YN = {
-    "y": True,
-    "yes": True,
-    "n": False,
-    "no": False,
-}
-
-
-INIT_GIT = YN.get("{{ cookiecutter.init_git|lower }}", True)
-
 ROOT = os.path.abspath(os.curdir)
-
 
 def run(cmd, check=True):
     print("> " + " ".join(cmd))
@@ -23,8 +11,6 @@ def run(cmd, check=True):
         print(f"! Skipping: command not found: {cmd[0]}")
         return subprocess.CompletedProcess(cmd, 0)
 
-
-if INIT_GIT:
-    run(["git", "init", "-b", "main"])  # falls back if -b unsupported
-    run(["git", "add", "."])
-    run(["git", "commit", "-m", "chore: scaffold from cookiecutter"])
+run(["git", "init", "-b", "main"])  # falls back if -b unsupported
+run(["git", "add", "."])
+run(["git", "commit", "-m", "chore: scaffold from cookiecutter"])
